df75d045031788d9f94b2da1b47c51d0a1cc9b76
Max <stopandgo31@gmail.com>
2021-06-11 04:47:01 +0300

    init-config advanced

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const production = (process.env.WEBPACK_ENV || process.env.NODE_ENV) === 'production'
+const ESLintPlugin = require('eslint-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
+const filename = ext => (production ? `bundle.[fullhash].${ext}` : `bundle.${ext}`);
 
 module.exports = {
   mode: production ? 'production' : 'development',
   entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    filename: filename('js')
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js', '.scss' ],
+    extensions: [ '.tsx', '.ts', '.js', '.scss' ]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        use: 'ts-loader'
       },
       {
         test: /\.s[ac]ss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
-      },
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      }
     ]
+  },
+  devtool: production ? false : 'source-map',
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new ESLintPlugin({
+      extensions: [ 'js', 'jsx', 'ts', 'tsx' ]
+    }),
+    new CleanWebpackPlugin()
   ]
 };
