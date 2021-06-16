f4f0788a7bebe5b9e8eabcbbcb3bf5de1de7b90e
Igor Shipulin <40541530+SupergroverNN@users.noreply.github.com>
2021-06-16 16:01:38 +0300

    fix: change route contants name

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Input from 'components/input/Input';
--- src/pages/profileEdit/ProfileEdit.tsx
+++ src/pages/profileEdit/ProfileEdit.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from 'components/button/Button';
