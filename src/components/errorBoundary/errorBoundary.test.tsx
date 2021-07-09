f6f9ab35ed416a3d711752190038a6434c3c6516
Igor Shipulin <40541530+SupergroverNN@users.noreply.github.com>
2021-07-09 21:10:36 +0300

    fix: PR issues 2

--- src/components/errorBoundary/errorBoundary.test.tsx
+++ src/components/errorBoundary/errorBoundary.test.tsx
@@ -2,25 +2,19 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 
 import ErrorBoundary from './ErrorBoundary';
+import { Throwable } from './mock';
 
 describe('test ErrorBoundary', () => {
-
   it('check render w/o error', () => {
     const testText = 'no error children text';
     const component = shallow(<ErrorBoundary>{testText}</ErrorBoundary>);
 
     expect(component.text()).toEqual(testText);
-
     expect(component).toMatchSnapshot();
   });
-
   it('check render with error', () => {
-    const Throwable: React.FC = (): never => {
-      throw new Error('Oops...');
-    };
-    const component = shallow(<ErrorBoundary>{Throwable}</ErrorBoundary>);
-    console.log(component.debug());
+    const component = shallow(<ErrorBoundary><Throwable /></ErrorBoundary>);
+
     expect(component).toMatchSnapshot();
   });
-
 });
--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -4,29 +4,26 @@ import { shallow } from 'enzyme';
 import Input from './Input';
 
 describe('test Input', () => {
-
   it('check render component with title', () => {
     const component = shallow(<Input
       value='testValue'
       name='testName'
       title='testTitle'
     />);
-    expect(component.text()).toEqual('testTitle');
 
+    expect(component.text()).toEqual('testTitle');
     expect(component).toMatchSnapshot();
   });
-
   it('check render component w/o value', () => {
     const component = shallow(<Input></Input>);
+
     expect(component).toMatchSnapshot();
   });
-
   it('check render component with type', () => {
     const component = shallow(<Input type='password'></Input>);
-    expect(component.find('input').get(0).props.type).toEqual('password');
+
     expect(component).toMatchSnapshot();
   });
-
   it('check render component with onChange', () => {
     const mockOnChange = jest.fn();
     const component = shallow(<Input
@@ -35,9 +32,7 @@ describe('test Input', () => {
       title='testTitle'
       onChange={mockOnChange}
     />);
-    component.find('input').simulate('change');
-    expect(mockOnChange.mock.calls.length).toEqual(1);
+
     expect(component).toMatchSnapshot();
   });
-
 });
