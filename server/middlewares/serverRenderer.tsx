6f853d485ea12b02df80f06ff9fed964049e7bf0
Max <stopandgo31@gmail.com>
2021-07-26 12:46:40 +0300

    Viewport fix

--- server/middlewares/serverRenderer.tsx
+++ server/middlewares/serverRenderer.tsx
@@ -20,6 +20,7 @@ const getHtml = (
       <html lang="en">
         <head>
             <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
             <link rel="stylesheet" href="${CLIENT_BUNDLE_NAME}.css">
             ${helmetData.title.toString()}
             ${helmetData.meta.toString()}
