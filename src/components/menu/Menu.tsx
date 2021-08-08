f2d2e089ad3470935fc875f44f39f4efce3aa82f
Igor Shipulin <40541530+SupergroverNN@users.noreply.github.com>
2021-08-08 21:31:53 +0300

    feat: add menuItem component

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,11 +1,11 @@
 import React, { FC, ReactElement } from 'react';
-import { Link } from 'react-router-dom';
 import cn from 'classnames';
 
-import { GAME_NAME, TOGGLE_ITEMS } from 'constants/commonConstants';
+import { GAME_NAME } from 'constants/commonConstants';
 import { MENU_ITEMS } from 'constants/menuConstants';
 
 import './menu.scss';
+import MenuItem from './MenuItem';
 
 export type TMenuItem = {
   title: string;
@@ -45,40 +45,15 @@ const Menu: FC<IMenu> = ({
       {isWithTitle && <h1>{GAME_NAME}</h1>}
       <div className='menu-items'>
         {
-          menuItems.map(({ title, route, action, mobileOnly, withModifier }) => {
-            if (mobileOnly && !isMobile) {
-              return;
-            }
-
-            const callback = () => {
-              if (action && handleAction) {
-                handleAction(action);
-              }
-            };
-
-            if (route) {
-              return (
-                <Link key={title} className='menu-item' to={route} onClick={action ? callback : undefined}>
-                  {title}
-                </Link>
-              );
-            }
-
-            if (action) {
-              const mode = modifier[title] ? 'ON' : 'OFF';
-
-              return (
-                <span key={title} className='menu-item' onClick={callback}>
-                  {`${title}${withModifier ? ':' : ''}`}
-                  {withModifier
-                    && <span className={`menu-item_mode_${mode.toLowerCase()}`}>{TOGGLE_ITEMS[mode]}</span>
-                  }
-                </span>
-              );
-            }
-
-          })
-        }
+          menuItems.map((item: TMenuItem) => (
+            <MenuItem
+              key={item.title}
+              item={item}
+              modifier={modifier}
+              isMobile={isMobile}
+              handleAction={handleAction}
+            />)
+          )}
       </div>
     </div>
 
