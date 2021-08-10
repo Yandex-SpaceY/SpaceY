57dd4bd175673c17617129ce056ce65460c2f8a1
SupergroverNN <booger1@yandex.ru>
2021-08-10 22:06:57 +0300

    fix: PR issues 2

--- src/constants/alertConstants.ts
+++ src/constants/alertConstants.ts
@@ -1,9 +1,9 @@
 export const ALERT_TEXTS = {
-  CHANGE_PASSWORD: 'Change Password',
-  LEADERBOARD: 'Get Leaders',
-  LOGIN: 'Sign In',
-  YANDEX_LOGIN: 'Yandex Auth',
-  PROFILE: 'Get User Data',
-  SIGNUP: 'Profile Creating',
-  PROFILE_EDIT: 'Profile Edit',
+  CHANGE_PASSWORD: 'Change password',
+  LEADERBOARD: 'Get leaders',
+  LOGIN: 'Sign in',
+  YANDEX_LOGIN: 'Yandex auth',
+  PROFILE: 'Get user data',
+  SIGNUP: 'Profile creating',
+  PROFILE_EDIT: 'Profile edit',
 };
