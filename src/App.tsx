78f3a6950caf51cc6dbfdb551f03f50c2936621c
Max <stopandgo31@gmail.com>
2021-08-12 00:11:10 +0300

    Spinner output

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { FC, ReactElement } from 'react';
 
-import { ErrorBoundary } from 'components';
+import { ErrorBoundary, Spinner } from 'components';
 import Router from 'router/Router';
 import { hot } from 'react-hot-loader/root';
 
 const App: FC = (): ReactElement => (
   <ErrorBoundary>
     <Router />
+    <Spinner />
   </ErrorBoundary>
 );
 
