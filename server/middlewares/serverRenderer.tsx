3c0c781ed6d55e5b584250d186e248db3a8f53a1
Max <stopandgo31@gmail.com>
2021-08-31 04:00:57 +0300

    nonce test

--- server/middlewares/serverRenderer.tsx
+++ server/middlewares/serverRenderer.tsx
@@ -38,8 +38,7 @@ const getHtml = (
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-            <meta property="csp-nonce" content="${nonce}">
-            <meta http-equiv="Content-Security-Policy" content="font-src 'self'">
+            
             <link nonce="${nonce}" rel="stylesheet" href="${styleFileName}">
             ${helmetData.title.toString()}
             ${helmetData.meta.toString()}
